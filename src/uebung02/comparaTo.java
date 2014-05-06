d62fa734892f4be18f67b078f6c1629cf70ae59f
nurgul <nurgul@192.168.33.34>
2014-05-06 18:34:29 +0200

    Added

---
+++ src/uebung02/comparaTo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uebung02;
+
+
+public class comparaTo extends Auto {
+
+    public comparaTo(short reifen, String farbe, short ps, short tueren, short airbags) {
+        super(reifen, farbe, ps, tueren, airbags);
+    }
+    
+}
